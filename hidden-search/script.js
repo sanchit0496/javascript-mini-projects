fde8ca79c53ccf4fc68dd4cfcce780c11dcc9884
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2021-05-09 10:13:01 +0530

    Create script.js

---
+++ hidden-search/script.js
@@ -0,0 +1,7 @@
+let search = document.querySelector('.search');
+let btn = document.querySelector('.btn');
+let input = document.querySelector('.input');
+
+btn.addEventListener('click', function(){
+    search.classList.toggle('active');
+});
