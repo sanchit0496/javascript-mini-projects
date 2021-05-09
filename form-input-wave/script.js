0ff48b08124dc942ab238621cf27db0e1dd4252c
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2021-05-09 18:39:11 +0530

    Create script.js

---
+++ form-input-wave/script.js
@@ -0,0 +1,8 @@
+let labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 40}ms">${letter}</span>`)
+        .join('')
+})
