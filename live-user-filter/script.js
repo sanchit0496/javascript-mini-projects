e6fb8dfd734cc1c23455b9b9321c17c6c1ca320c
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2021-05-10 18:08:00 +0530

    Update script.js

--- live-user-filter/script.js
+++ live-user-filter/script.js
@@ -1,4 +1,7 @@
 let output = document.getElementById('output');
+let filter = document.getElementById('filter');
+let names = [];
+filter.addEventListener('input', (e) => filterData(e.target.value));
 
 async function getUser(){
     let a = await fetch('https://randomuser.me/api?results=15');
@@ -6,17 +9,31 @@ async function getUser(){
     return b;
 }
 
-
-
 let ou = "";
-
+let res = "";
 async function add(){
     let z = await getUser();
-    console.log(z);
     for(let i = 0; i < 15; i++){
-        ou += `<li>${z.results[i].name.first}</li>`;
+        ou += `<li class = "show">${z.results[i].name.first}</li>`;
+        names.push(`${z.results[i].name.first}`);
+    }
+    output.innerHTML = ou;
+    let res = document.querySelectorAll('.show');
+    console.log(names.length);
+}
+
+function filterData(searchTerm){
+    ou = "";
+    for(let i = 0; i < names.length; i++){
+        if(names[i].indexOf(searchTerm) > -1){
+            ou += `<li style = "color:white;">${names[i]}</li>`;
+        }else{
+        }
     }
     output.innerHTML = ou;
 }
+
+
+
 getUser();
 add();
