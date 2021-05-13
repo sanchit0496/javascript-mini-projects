2c0c695b97dc6db6b9b256f79545979e001d85ae
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2021-05-14 00:13:24 +0530

    Create script.js

---
+++ split-landing-page/script.js
@@ -0,0 +1,17 @@
+let left = document.querySelector('.left');
+let right = document.querySelector('.right');
+let container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left');
+})
+left.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-left');
+})
+
+right.addEventListener('mouseenter', () => {
+    container.classList.add('hover-right');
+})
+right.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-right');
+})
