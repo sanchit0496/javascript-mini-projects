ff3512a8b08dd757f5d67aafdf489a6ec4578822
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2021-05-15 09:27:35 +0530

    Create script.js

---
+++ animated-countdown/script.js
@@ -0,0 +1,40 @@
+let nums = document.querySelectorAll('.nums span')
+let counter = document.querySelector('.counter')
+let finalMessage = document.querySelector('.final')
+let replay = document.querySelector('#replay')
+
+runAnimation()
+
+function resetDOM() {
+  counter.classList.remove('hide')
+  finalMessage.classList.remove('show')
+
+  nums.forEach((num) => {
+    num.classList.value = ''
+  })
+
+  nums[0].classList.add('in')
+}
+
+function runAnimation() {
+  nums.forEach((num, idx) => {
+    let nextToLast = nums.length - 1
+
+    num.addEventListener('animationend', (e) => {
+      if (e.animationName === 'goIn' && idx !== nextToLast) {
+        num.classList.remove('in')
+        num.classList.add('out')
+      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+        num.nextElementSibling.classList.add('in')
+      } else {
+        counter.classList.add('hide')
+        finalMessage.classList.add('show')
+      }
+    })
+  })
+}
+
+replay.addEventListener('click', () => {
+  resetDOM()
+  runAnimation()
+})
